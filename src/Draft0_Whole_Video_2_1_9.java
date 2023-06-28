9963c2135cc775176ed484813cce08687bc14948
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-06-28 08:42:21 +0300

    Initial commit

--- src/Draft0_Whole_Video_2_1_9.java
+++ src/Draft0_Whole_Video_2_1_9.java
@@ -1,5 +1,7 @@
-public class Draft1_Task2_1_9 {
+public class Draft0_Whole_Video_2_1_9 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание : \n1.\tУ вас есть список моделей смартфонов, iPhone 6 S\", \"Lumia 950\", \"Samsung\n" +
+                "Galaxy S 6\", \"LG G 4\", \"Nexus 7\", вам нужно вывести в консоль названия двух моделей\n" +
+                "пропустив при этом первый бренд при помощи метода skip и limit\n\nРешение: ");
     }
 }
--- src/Draft1_Task2_1_9.java
+++ src/Draft1_Task2_1_9.java
@@ -1,5 +1,8 @@
-public class Main {
+public class Draft1_Task2_1_9 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание : \n1.\tУ вас есть список моделей смартфонов, iPhone 6 S\", \"Lumia 950\", \"Samsung\n" +
+                "Galaxy S 6\", \"LG G 4\", \"Nexus 7\", вам нужно вывести в консоль названия двух моделей\n" +
+                "пропустив при этом первый бренд при помощи метода skip и limit\n\nРешение: ");
+
     }
 }
